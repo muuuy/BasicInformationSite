61a2f8ce85b095ef938c247b22e6a646a2ee96b7
Matthew Yu <myu2@umbc.edu>
2024-05-23 12:04:01 -0400

    final commit

--- index.js
+++ index.js
@@ -18,9 +18,8 @@ const server = http.createServer((req, res) => {
             });
         } else {
             res.writeHead(200, { 'Content-Type': 'text/html' });
-            var q = url.parse(req.url, true).query;
-            var txt = q.year + " " + q.month;
-            res.end(txt);
+            res.write(req.url);
+            res.end(content, 'utf-8');
         }
     });
 });
